91cb4efb2a2ae35ca5b4186cfe22a47156ab68c8
Gytha Ogg <gythaoggscat@gmail.com>
2024-05-22 10:42:16 +0200

    initial commit
    Empty apis instance running

---
+++ README.md
@@ -0,0 +1,1 @@
+apis-instance-nomansland-dev
---
+++ apis_ontology/settings/server_settings.py
@@ -0,0 +1,55 @@
+from apis_acdhch_default_settings.settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+APIS_BASE_URI = "https://nomansland.acdh-ch-dev.oeaw.ac.at/"
+
+ROOT_URLCONF = "apis_ontology.urls"
+CSRF_TRUSTED_ORIGINS = [
+    "https://nomansland.acdh-ch-dev.oeaw.ac.at",
+]
+
+INSTALLED_APPS += [
+    "apis_highlighter",
+    "django.contrib.postgres",
+    "apis_core.collections",
+    "apis_core.history",
+    "django_acdhch_functions",
+]
+INSTALLED_APPS.remove("apis_ontology")
+INSTALLED_APPS.insert(0, "apis_ontology")
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(levelname)-8s %(asctime)s] %(name)-6s %(message)s",
+            "datefmt": "%y-%m-%d %H:%M %Z",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
+LOG_LIST_NOSTAFF_EXCLUDE_APP_LABELS = ["admin", "sessions", "auth"]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    },
+    "select2": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "TIMEOUT": 60 * 60 * 24,  # Timeout set to 1 day (in seconds)
+    },
+}
