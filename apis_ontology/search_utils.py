81a3fcc790ef91a47c9dc3aa3043ccd80fb680c0
sennierer <matthias.schloegl@oeaw.ac.at>
2025-12-05 10:53:15 +0100

    fix: query on RootObject and add vectors to global search
    querying on RootObject and annotating the needed fields from the
    entities tables produces only one query. Adding vectors increases
    performance and allows to rank/weight. However, this does not allow to
    filter for partial words, so might not be enough.

--- apis_ontology/search_utils.py
+++ apis_ontology/search_utils.py
@@ -1,17 +1,23 @@
-from itertools import chain
-
-from apis_core.apis_entities.models import AbstractEntity
 from apis_core.apis_entities.utils import get_entity_classes
-from apis_core.generic.helpers import default_search_fields, generate_search_filter
-from apis_core.relations.models import Relation
-from django.db.models import F, Model, OuterRef, Q, QuerySet, Value
+from apis_core.apis_metainfo.models import RootObject
+from apis_core.generic.helpers import default_search_fields
+from django.contrib.postgres.search import (
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+)
+from django.db.models import CharField, F, Model, OuterRef, Q, QuerySet, Value
 from django.db.models.functions import Concat
 
 
 def annotate_qs(qs: QuerySet, model: type[Model], fields: list[str]) -> QuerySet:
-    field_exprs = [item for field in fields for item in [F(field), Value(" ")]][:-1]
+    field_exprs = [item for field in fields for item in [F(field.name), Value(" ")]][
+        :-1
+    ]
     m = model.objects.filter(pk=OuterRef("pk")).annotate(
-        search_obj=Concat(*field_exprs)
+        search_obj=Concat(*field_exprs, output_field=CharField())
+        if len(field_exprs) > 1
+        else F(fields[0].name)
     )
     annotation = {
         f"gs_{model.__name__.lower()}": m.values_list("search_obj", flat=True)
@@ -23,7 +29,28 @@ def annotate_qs(qs: QuerySet, model: type[Model], fields: list[str]) -> QuerySet
 def search(query: str, user: object):
     # search in entities:
     entities = get_entity_classes()
-    qs = AbstractEntity.objects.all()
+    qs = RootObject.objects_inheritance.all()
+    entities_search_vector = []
     for entity in entities:
-        qs = annotate_qs(qs, entity, default_search_fields(entity))
-    return qs
+        if user.has_perm(entity.get_view_permission()):
+            qs = annotate_qs(qs, entity, default_search_fields(entity))
+            entities_search_vector.append(entity)
+    if not entities_search_vector:
+        return qs.none()
+    search_vector = SearchVector(
+        *[
+            f"gs_{entity.__name__.lower()}"
+            for entity in entities
+            if entity in entities_search_vector
+        ]
+    )
+    qs = qs.annotate(
+        search_vector=search_vector,
+        rank=SearchRank(search_vector, query),
+    )
+    search_query = SearchQuery(
+        query,
+        search_type="websearch",
+    )
+    res = qs.filter(search_vector=search_query).order_by("-rank")
+    return res.select_subclasses()
