226e7a1ebdcf0e3ecb394f23fbb9263f94bc8d46
Gytha Ogg <gythaoggscat@gmail.com>
2025-12-11 08:12:23 +0100

    feat: map visualisation of entities

--- apis_ontology/views.py
+++ apis_ontology/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import FormView
+from django.shortcuts import render
 
 from apis_ontology.forms import SearchForm
 from apis_ontology.search_utils import search
@@ -16,3 +17,7 @@ class SearchView(FormView):
         if query := self.request.GET.get("search"):
             context["objects"] = search(query, self.request.user)
         return context
+
+
+def map_view(request):
+    return render(request, "apis_ontology/mapviz.html")
